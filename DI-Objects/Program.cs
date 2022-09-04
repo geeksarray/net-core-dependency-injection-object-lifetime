c2d04fc1369d0308e9e62c833822320fcf53cbc2
laxmikant <laxmikant.lad@outlook.com>
2022-09-03 20:57:02 -0400

    added solution

---
+++ DI-Objects/Program.cs
@@ -0,0 +1,66 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DI_Objects;
+using Microsoft.Extensions.DependencyInjection;
+
+//var serviceProvider = new ServiceCollection()
+//            .AddSingleton<IService, Service>()
+//            .BuildServiceProvider();
+
+//Console.WriteLine("   Dependency Injection Singleton");
+//var serviceObject1 = serviceProvider.GetService<IService>();
+//Console.WriteLine($"   Request 1 {serviceObject1.GetObjectCreationDateTime} ");
+
+//for (int i = 0; i < 5000; i++) ;
+
+//var serviceObject2 = serviceProvider.GetService<IService>();
+//Console.WriteLine($"   Request 2 {serviceObject2.GetObjectCreationDateTime} ");
+
+
+//for (int i = 0; i < 5000; i++) ;
+//var serviceObject3 = serviceProvider.GetService<IService>();
+//Console.WriteLine($"   Request 3 {serviceObject3.GetObjectCreationDateTime} ");
+
+
+//var serviceProvider = new ServiceCollection()
+//            .AddTransient<IService, Service>()
+//            .BuildServiceProvider();
+
+//Console.WriteLine(" Dependency Injection Transient");
+//var serviceObject1 = serviceProvider.GetService<IService>();
+//Console.WriteLine($" Request 1 {serviceObject1.GetObjectCreationDateTime} ");
+
+//for (int i = 0; i < 5000; i++) ;
+
+//var serviceObject2 = serviceProvider.GetService<IService>();
+//Console.WriteLine($" Request 2 {serviceObject2.GetObjectCreationDateTime} ");
+
+
+//for (int i = 0; i < 5000; i++) ;
+//var serviceObject3 = serviceProvider.GetService<IService>();
+//Console.WriteLine($" Request 3 {serviceObject3.GetObjectCreationDateTime} ");
+
+
+var serviceProvider = new ServiceCollection()
+            .AddScoped<IService, Service>()
+            .BuildServiceProvider();
+
+
+Console.WriteLine("   Dependency Injection Scoped");
+Console.WriteLine("   Scope 1");
+
+using (var scope = serviceProvider.CreateScope())
+{
+    Console.WriteLine($"   Request 1 {scope.ServiceProvider.GetService<IService>().GetObjectCreationDateTime}");
+    for (int i = 0; i < 5000; i++);
+    Console.WriteLine($"   Request 2 {scope.ServiceProvider.GetService<IService>().GetObjectCreationDateTime}");
+}
+
+Console.WriteLine("");
+Console.WriteLine("   Scope 2");
+using (var scope = serviceProvider.CreateScope())
+{
+    Console.WriteLine($"   Request 1 { scope.ServiceProvider.GetService<IService>().GetObjectCreationDateTime }" );
+    for (int i = 0; i < 5000; i++) ;
+    Console.WriteLine($"   Request 2 { scope.ServiceProvider.GetService<IService>().GetObjectCreationDateTime } ");
+}
+
---
+++ DI-Objects/Service.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DI_Objects
+{
+    internal class Service : IService
+    {
+        string _currentDateTime;
+        public Service()
+        {   
+            this._currentDateTime = DateTime.Now.ToString("MMMM-dd hh.mm.ss.ffffff");
+        }
+
+        public string GetObjectCreationDateTime { get { return this._currentDateTime; } }
+       
+    }
+}
